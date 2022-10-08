c7335b9c83245dcd6b0e11c1e4eec639fe9c1caa
BetoCarrillo <alberto.carrillo01@gmail.com>
2022-10-08 09:18:38 -0400

    tree card working

--- client/src/Views/Home.js
+++ client/src/Views/Home.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/home.css";
 import "../App.css";
 import ModalHome from "../Components/ModalHome";
 
 function Home() {
+  const [userProfile, setUserProfile] = useState({});
+  const [error, setError] = useState(null);
+  const getProfile = async () => {
+    const token = localStorage.getItem("token");
+    console.log("token", token);
+
+    if (token) {
+      const myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+      };
+      try {
+        const response = await fetch(
+          "http://localhost:5005/api/users/profile",
+          requestOptions
+        );
+        const result = await response.json();
+        setUserProfile({
+          email: result.email,
+          userName: result.userName,
+          avatarPicture: result.avatarPicture,
+        });
+      } catch (error) {
+        console.log("error getting user's profile", error);
+      }
+    } else {
+      setError(true);
+      console.log("no token for this user");
+    }
+  };
+
   return (
     <div className="homeDiv">
       <div className="homeTitle ">
--- client/src/Views/Trees.js
+++ client/src/Views/Trees.js
@@ -1,23 +1,42 @@
 import React from "react";
 import useFetch from "../hooks/useFetch";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 function Trees() {
   const { data, loading, error } = useFetch(
     "http://localhost:5005/api/trees/all/"
   );
-  console.log("data", data);
+
+  console.log("data", data, data.i);
+
+  let date = new Date(data.allTrees[0].date).toLocaleString();
+  console.log("date", date);
 
   return (
     <div>
-      {data &&
-        data.allTrees.map((value, i) => {
-          return (
-            <div key={i}>
-              <h2>{value.name}</h2>
-            </div>
-          );
-        })}
-      {data && <p>{data.allTrees[0].name}</p>}
+      {!loading
+        ? data &&
+          data.allTrees.map((tree, i) => {
+            return (
+              <div key={i}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={tree.img} height="280rem" />
+                  <Card.Body>
+                    <Card.Title>{tree.name}</Card.Title>
+                    <Card.Text>
+                      Location: {tree.location}
+                      <br />
+                      Type: {tree.type} <br />
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            );
+          })
+        : "...loading adopted trees..."}
+      {/*   {data && <p>{data.allTrees[0].name}</p>} */}
     </div>
   );
 }
